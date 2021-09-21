f732990f16137365b315a4db7e05762b679897f5
Scout McFall <scoutmknz@gmail.com>
2021-09-21 19:40:31 +0000

    loop create divs for chars

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -19,7 +19,11 @@ let numWrong = 0;
 // Loop over the chars in `word` and create divs.
 //
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const char in word){
+    const letter = char;
+    $('#word-container').append
+    (`<div class="letter-box ${letter}"></div>`);
+  }
 };
 
 // Loop over each letter in `ALPHABET` and generate buttons.
